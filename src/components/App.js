c87a0ed1c5495d69f8030d11dbc44eb733b6ebe7
Rachna <rachnaprakash.balani@walmartlabs.com>
2019-08-25 00:23:33 -0700

    artist image on search

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React , { Component } from 'react';
+import Artist from './Artist';
 
 const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
 
@@ -59,6 +60,7 @@ class App extends Component {
             placeholder='Search artist here' 
             />
            <button onClick={this.searchArtist}>Search</button>
+        <Artist artist={this.state.artist} />
         </div>
         )
     }
