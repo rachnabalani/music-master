c10677744551c4b06cf51ebdb7a52c5b9a7ad230
Rachna <rachnaprakash.balani@walmartlabs.com>
2019-08-24 21:57:17 -0700

    search button

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 import React , { Component } from 'react';
 
+
 class App extends Component {
- 
+    state = { artistQuery: '' };
+
+    updateArtistQuery = event => {
+        this.setState({artistQuery: event.target.value });
+    }
+
+    searchArtist = () => {
+        console.log('this.state', this.state);
+    }
+
+    handleKeyPress = event => {
+        if(event.key === 'Enter'){
+            this.searchArtist();
+        }
+    }
+
     render () {
         return (
         <div> 
-           React App
+           <h2> Music Master </h2>
+           <input
+            onChange={this.updateArtistQuery}
+            onKeyPress={this.handleKeyPress}
+            placeholder='Search artist here' 
+            />
+           <button onClick={this.searchArtist}>Search</button>
         </div>
         )
     }
