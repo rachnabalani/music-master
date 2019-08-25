9dc7dc0b8a626ea711775baf40735f81fe9370eb
Rachna <rachnaprakash.balani@walmartlabs.com>
2019-08-25 00:08:52 -0700

    search refined

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,45 @@
 import React , { Component } from 'react';
 
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+
+
 
 class App extends Component {
-    state = { artistQuery: '' };
+    state = { 
+        artistQuery: '' ,
+        artist: null,
+        tracks: []
+    };
 
     updateArtistQuery = event => {
         this.setState({artistQuery: event.target.value });
     }
 
     searchArtist = () => {
-        console.log('this.state', this.state);
+       
+        fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+        .then(response => response.json())
+        .then(json => {
+
+            if(json.artists.total >0){
+                const artist = json.artists.items[0];
+                this.setState({ artist: artist});
+                
+                //make a request for top tracks
+                fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                .then(response => response.json())
+                .then (json => {
+                    this.setState({tracks: json.tracks});
+                })
+                .catch(error => alert(error.message));
+
+            }
+            else{
+                console.log('artist not found');
+            }
+        })
+        .catch(error => alert(error.message));
+        
     }
 
     handleKeyPress = event => {
@@ -19,6 +49,7 @@ class App extends Component {
     }
 
     render () {
+        console.log('this.state', this.state);
         return (
         <div> 
            <h2> Music Master </h2>
